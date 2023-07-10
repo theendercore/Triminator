3f9d5650f12b29fdc0cec32b583da25009b7f21b
theendercore <theendercore@gmail.com>
2023-07-10 18:03:36 +0300

    fixed whipe bug

--- src/app.tsx
+++ src/app.tsx
@@ -39,7 +39,7 @@ export default function App() {
               await sleep(100);
               // @ts-ignore
               clickThing2.current.click();
-              setVal(nullObj);
+              // setVal(nullObj);
             });
           }
 
@@ -49,7 +49,7 @@ export default function App() {
               await sleep(100);
               // @ts-ignore
               clickThing.current.click();
-              setVal(nullObj);
+              // setVal(nullObj);
             });
           }
         }}
