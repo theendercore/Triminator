331e90782ad80229f4a6c15427bc6cd2d84d4cc1
theendercore <theendercore@gmail.com>
2024-01-15 08:35:57 +0200

    - materials now display in nicer rows

--- src/components/gen/v1/Material.tsx
+++ src/components/gen/v1/Material.tsx
@@ -14,25 +14,27 @@ type MaterialProps = {
 export default function Material({material, remove, edit, isOpen, advanced}: MaterialProps) {
 
     return (
-        <div class="grid place-items-center bg-secondary bg-opacity-40 px-8 py-4 rounded-3xl relative">
-            <span className="flex flex-col md:flex-row md:gap-3 items-center">
+        <div
+            className="grid columns-2 grid-flow-col-dense auto-cols-auto justify-end gap-4 md:gap-8 xl:gap-16 bg-secondary bg-opacity-40 px-8 py-4 rounded-3xl relative">
+            <span className="justify-self-end flex flex-row md:gap-3 items-center re">
                 <span>
                     {material.translation}
-                    <span className={`text-text opacity-50 ${ advanced ? "inline" : "hidden"}`}>{`(${material.name})`}</span>
+                    <span
+                        className={`text-text opacity-50 ${advanced ? "inline" : "hidden"}`}>{`(${material.name})`}</span>
                 </span>
 
                 <span class="hidden md:inline">|</span>
                 <ItemRender item={`minecraft:${material.item}`} width={32} height={32}/>
                 <span class="hidden md:inline">|</span>
 
-                  {material.palletTexture &&
-                      <img src={material.palletTexture}
-                           class="pixel-art text-center" height={16} width={128} alt=""/>
-                  }
+                {material.palletTexture &&
+                    <img src={material.palletTexture}
+                         class="pixel-art text-center" height={16} width={128} alt=""/>
+                }
             </span>
 
-
-            <div class="flex gap-2 absolute right-3 ">
+            <div className="right-3 w-16"></div>
+            <div className="flex gap-2 absolute right-3 self-center ">
                 <SecondaryButton
                     className={isOpen ? "cursor-not-allowed hover:scale-100" : " "}
                     onClick={() => (!isOpen) && edit(material.id)}
